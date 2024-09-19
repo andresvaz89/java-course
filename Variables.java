9f1ed9cde77f8de19a8020b830159de79bc3de29
andresvaz89 <andres.vazquez.martin@gmail.com>
2024-09-19 12:24:17 +0200

    first comit

---
+++ Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int x = 78;
+        byte a = 122;
+        short m = 32766;
+        long e = 132846793824567L;
+        float b = 3.18778f;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Andres";
+
+        System.out.println(x);
+        System.out.println(a);
+        System.out.println(m);
+        System.out.println(e);
+        System.out.println(b);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(symbol);
+        System.out.println("Hola mi nombre es: " + name); 
+
+    }
+}
