da28edc2158d42585ffe59fab5b9e7323358d673
andresvaz89 <andres.vazquez.martin@gmail.com>
2024-09-26 12:02:49 +0200

    cambiar string a número y sumarle 10 completado

---
+++ ConvertirCadenaAEntero.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ConvertirCadenaAEntero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un número (esto será en tipo de dato string):");
+        String numeroEnString = scanner.nextLine();
+
+        int numeroConParseInt = Integer.parseInt(numeroEnString);
+
+        numeroConParseInt += 10;
+
+        System.out.println("El número " +numeroEnString +" más 10 es: " + numeroConParseInt);
+
+        scanner.close();
+    }
+}
